1cf2ea51e93422b014f5c744fbcc57dd890557f9
Petr Pikaus <pikaus.petr@gmail.com>
2023-05-18 12:36:09 +0200

    Add project files.

---
+++ BackNavigationRepro/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace BackNavigationRepro;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+        Routing.RegisterRoute(nameof(NewPage1), typeof(NewPage1));
+	}
+}
---
+++ BackNavigationRepro/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace BackNavigationRepro;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private async void OnNavigatorClicked(object sender, EventArgs e)
+	{
+        await Shell.Current.GoToAsync(nameof(NewPage1));
+	}
+}
+
